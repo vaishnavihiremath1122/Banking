dce567c2190f7fd08d265a6f3890ffaae4bfd2c8
SAMEER KUMAR <114007933+Sameerst95@users.noreply.github.com>
2024-04-18 17:40:05 +0530

    Commit from Sameer

--- practiceProject/src/practiceProject/practice.java
+++ practiceProject/src/practiceProject/practice.java
@@ -5,6 +5,8 @@ public class practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hii");
+		
+		System.out.println("Hello Sameer....");
 
 	}
 
